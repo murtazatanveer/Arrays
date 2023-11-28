9b98f02e57ccce4aa4a3b94c6d94187ad1530335
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-11-29 03:34:36 +0500

    programs using arrays

---
+++ printing numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+int arr[5];
+
+for(int i=0;i<=34;i++){
+
+printf("\nEnter digit at index %d : ",i);
+scanf("%d",&arr[i]);
+
+}
+
+printf("\n______________\n");
+
+for(int i=0;i<=4;i++){
+
+printf("\n%d",arr[i]);
+
+}
+
+}
